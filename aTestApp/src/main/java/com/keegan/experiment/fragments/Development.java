d786bbdd9c11a0d6e2a2830c9a61dff2f68f80a1
Keegan <Keegan.Lew@youtap.com>
2016-01-29 10:43:29 +1300

    Development page initial

---
+++ aTestApp/src/main/java/com/keegan/experiment/fragments/Development.java
@@ -0,0 +1,42 @@
+package com.keegan.experiment.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.keegan.experiment.R;
+
+/**
+ * Created by Keegan on 28/01/16.
+ */
+public class Development extends Fragment implements View.OnClickListener {
+
+    private final String TAG = Development.class.getSimpleName();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_development, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
