54c59c2e6d9b339eb26118b3368a2a5ff30f0efe
KeeganLiew <Keegan.Liew@youtap.com>
2016-02-10 14:51:33 +1300

    Added UI feedback for Login Screen

--- aTestApp/src/main/java/com/keegan/experiment/activities/LoginActivity.java
+++ aTestApp/src/main/java/com/keegan/experiment/activities/LoginActivity.java
@@ -5,31 +5,40 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.DrawerLayout.DrawerListener;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.keegan.experiment.Global;
+import com.keegan.experiment.Intents;
 import com.keegan.experiment.R;
 
 import java.util.Arrays;
@@ -49,6 +58,12 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
     private EditText usernameET;
     private EditText pinET;
 
+    private RelativeLayout popUpProgressBarDialogRL;
+    private TextView popUpProgressBarTextTV;
+
+    private RelativeLayout popUpButtonDialogRL;
+    private Button popUpButtonTextB;
+
     private TableLayout numericKeypad;
     private TextView num1;
     private TextView num2;
@@ -61,12 +76,16 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
     private TextView num9;
     private TextView numMenu;
     private TextView num0;
-    private ImageView numBackspace;
+    //private ImageView numBackspace;
+    private TextView numBackspace;
+    private View currencyPressedKey;
 
     private Activity mActivity;
     private Context mContext;
     private String finalUsername;
     private String finalPin;
+    private TextView[] numpadList;
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,14 +118,12 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         num8 = (TextView) findViewById(R.id.custom_numeric_keyboard_key_8);
         num9 = (TextView) findViewById(R.id.custom_numeric_keyboard_key_9);
         numMenu = (TextView) findViewById(R.id.custom_numeric_keyboard_key_menu);
-        numBackspace = (ImageView) findViewById(R.id.custom_numeric_keyboard_key_backspace);
+        numBackspace = (TextView) findViewById(R.id.custom_numeric_keyboard_key_backspace);
 
-        TextView[] zeroToNineList = new TextView[]{num0, num1, num2, num3, num4, num5, num6, num7, num8, num9};
-        for (TextView number : zeroToNineList) {
-            number.setOnClickListener(numpadListener);
+        numpadList = new TextView[]{num1, num2, num3, num4, num5, num6, num7, num8, num9, numMenu, num0, numBackspace};
+        for (TextView number : numpadList) {
+            number.setOnTouchListener(numpadListener);
         }
-        numMenu.setOnClickListener(numpadListener);
-        numBackspace.setOnClickListener(numpadListener);
 
         //nav draw
         mDrawerLayout = (DrawerLayout) findViewById(R.id.Activity_Login_DrawerLayout);
@@ -129,6 +146,14 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
             tempTV.setText(navMenuTexts[i]);
             navMenuList[i].setOnClickListener(this);
         }
+
+        //pop up progress bar dialog
+        popUpProgressBarDialogRL = (RelativeLayout) findViewById(R.id.Activity_Login_PopUpProgressBarDialog);
+        popUpProgressBarTextTV = (TextView) findViewById(R.id.Pop_Up_Progress_Bar_Dialog_TextView_Label);
+        //pop up button dialog
+        popUpButtonDialogRL = (RelativeLayout) findViewById(R.id.Activity_Login_PopUpButtonDialog);
+        popUpButtonTextB = (Button) findViewById(R.id.Pop_Up_Button_Dialog_Button);
+        popUpButtonTextB.setOnClickListener(this);
     }
 
     @Override
@@ -137,6 +162,23 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         mActivity = this;
         mContext = getApplicationContext();
         loadSavedPreferences();
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                String action = intent.getAction();
+                Log.d(TAG, "Received Intent: " + intent.getAction());
+                if (Intents.LOGIN_SUCCESS.equalsName(action)) {
+                    enableAndShowViews(true);
+                } else if (Intents.LOGIN_FAIL.equalsName(action)) {
+                    popUpProgressBarDialogRL.setVisibility(View.GONE);
+                    popUpButtonDialogRL.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, new IntentFilter(Intents.LOGIN_SUCCESS.toString()));
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, new IntentFilter(Intents.LOGIN_FAIL.toString()));
     }
 
     @Override
@@ -154,6 +196,9 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
             case R.id.Activity_Login_Contact:
                 closeDrawer();
                 break;
+            case R.id.Pop_Up_Button_Dialog_Button:
+                enableAndShowViews(true);
+                break;
         }
     }
 
@@ -165,11 +210,13 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
     @Override
     protected void onPause() {
         super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
     }
 
     //Listeners
@@ -203,6 +250,9 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         public void onDrawerSlide(View view, float v) {
             if (v * 100 > Global.hide_keyboard_login_drawer_percentage) {
                 if (pinET.hasFocus()) {
+                    if (currencyPressedKey != null) {
+                        currencyPressedKey.setBackgroundColor(ContextCompat.getColor(mContext, R.color.custom_numpad_color));
+                    }
                     lastFocused = pinET;
                 } else if (usernameET.hasFocus()) {
                     hideKeyboard(mActivity);
@@ -228,34 +278,35 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         }
     }
 
-    private OnClickListener numpadListener = new OnClickListener() {
+    private OnTouchListener numpadListener = new OnTouchListener() {
         @Override
-        public void onClick(View v) {
-            Integer[] data = {R.id.custom_numeric_keyboard_key_0,
-                    R.id.custom_numeric_keyboard_key_1, R.id.custom_numeric_keyboard_key_2, R.id.custom_numeric_keyboard_key_3,
-                    R.id.custom_numeric_keyboard_key_4, R.id.custom_numeric_keyboard_key_5, R.id.custom_numeric_keyboard_key_6,
-                    R.id.custom_numeric_keyboard_key_7, R.id.custom_numeric_keyboard_key_8, R.id.custom_numeric_keyboard_key_9,
-                    R.id.custom_numeric_keyboard_key_menu, R.id.custom_numeric_keyboard_key_backspace
-            };
-
-            Integer pinClicked = Arrays.asList(data).indexOf(v.getId());
-            if (pinClicked >= 0 && pinClicked <= 9) {
-                pinET.append(pinClicked.toString());
-            } else if (pinClicked == 10) {
-                openDrawer();
-            } else if (pinClicked == 11) {
-                if (pinET.getText().length() > 0) {
-                    int cursorPosition = pinET.getSelectionStart();
-                    //Log.d(TAG, "Pin Cursor: " + cursorPosition);
-                    if (cursorPosition > 0) {
-                        pinET.getText().delete(cursorPosition - 1, cursorPosition);
+        public boolean onTouch(View view, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                Integer pinClicked = Arrays.asList(Global.numpadKeysData).indexOf(view.getId());
+                if (pinClicked >= 0 && pinClicked <= 9) {
+                    pinET.append(pinClicked.toString());
+                } else if (pinClicked == 10) {
+                    openDrawer();
+                } else if (pinClicked == 11) {
+                    if (pinET.getText().length() > 0) {
+                        int cursorPosition = pinET.getSelectionStart();
+                        //Log.d(TAG, "Pin Cursor: " + cursorPosition);
+                        if (cursorPosition > 0) {
+                            pinET.getText().delete(cursorPosition - 1, cursorPosition);
+                        }
                     }
+                } else {
+                    Log.w(TAG, "Somehow pressed a non-numpad key");
                 }
-            } else {
-                Log.w(TAG, "Somehow pressed a non-numpad key");
+                currencyPressedKey = view;
+                view.setBackgroundColor(ContextCompat.getColor(mContext, R.color.custom_numpad_color_pressed));
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                //currencyPressedKey = null;
+                view.setBackgroundColor(ContextCompat.getColor(mContext, R.color.custom_numpad_color));
+                Log.d(TAG, "Current Pin: " + pinET.getText().toString());
+                pinLoginChecker(pinET);
             }
-            Log.d(TAG, "Current Pin: " + pinET.getText().toString());
-            pinLoginChecker(pinET);
+            return false;
         }
     };
 
@@ -265,27 +316,66 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
         if (pin.length() == 4) {
             finalUsername = usernameET.getText().toString();
             finalPin = pin;
-            //show pop up dialog
-
             //start logging in
             startLoginProcess();
         }
     }
 
     private void startLoginProcess() {
-        if (finalPin.equalsIgnoreCase("9090")) {
-            //if correct credentials
-            savePreferences("Username", finalUsername);
-            //start main activity with extra info
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            Bundle b = new Bundle();
-            b.putString("Username", finalUsername); //Your id
-            b.putString("Pin", finalPin); //Your id
-            b.putBoolean("OpenNavDraw", false); //Your id
-            intent.putExtras(b); //Put your id to your next Intent
-            startActivity(intent);
-            //finish();
-            //startActivity(new Intent(CustomerLoginActivity.this, ApplicationActivity.class).putExtra(Intents.EXTRA_ACCOUNT_BALANCE.toString(), accountBalance));
+        enableAndShowViews(false);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(2000);
+                    }
+                } catch (InterruptedException ex) {
+                    Log.d(TAG, ex.toString());
+                }
+                Intent intent;
+                if (finalPin.equalsIgnoreCase("9090")) {
+                    intent = new Intent(Intents.LOGIN_SUCCESS.toString());
+                    LocalBroadcastManager.getInstance(mActivity).sendBroadcast(intent);
+                    Log.d(TAG, "Sending Intent: " + intent.getAction());
+
+                    //if correct credentials
+                    savePreferences("Username", finalUsername);
+                    //start main activity with extra info
+                    intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Bundle b = new Bundle();
+                    b.putString("Username", finalUsername); //Your id
+                    b.putString("Pin", finalPin); //Your id
+                    b.putBoolean("OpenNavDraw", false); //Your id
+                    intent.putExtras(b); //Put your id to your next Intent
+                    startActivity(intent);
+                    //finish();
+                    //startActivity(new Intent(CustomerLoginActivity.this, ApplicationActivity.class).putExtra(Intents.EXTRA_ACCOUNT_BALANCE.toString(), accountBalance));
+                } else {
+                    intent = new Intent(Intents.LOGIN_FAIL.toString());
+                    LocalBroadcastManager.getInstance(mActivity).sendBroadcast(intent);
+                    Log.d(TAG, "Sending Intent: " + intent.getAction());
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private void enableAndShowViews(boolean bool) {
+        if (bool) {
+            popUpButtonDialogRL.setVisibility(View.GONE);
+            popUpProgressBarDialogRL.setVisibility(View.GONE);
+            pinET.setText(Global.EMPTY_STRING);
+        } else {
+            popUpProgressBarDialogRL.setVisibility(View.VISIBLE);
+            popUpProgressBarTextTV.setText(getString(R.string.logging_in));
+        }
+        setEnabledNumpad(bool);
+    }
+
+    private void setEnabledNumpad(boolean bool) {
+        for (TextView number : numpadList) {
+            number.setEnabled(bool);
         }
     }
 
@@ -311,6 +401,7 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
             }, 30);
         }
     }
+
 
     private void startFragment(Fragment mFragment) {
         if (mFragment == null) {
